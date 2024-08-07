c6c6b42acec8317a7fd521320457aaf82689da08
King William <seanapowi@gmail.com>
2024-08-07 16:49:35 +0300

    solve fizzbuzz function challenge

---
+++ app.js
@@ -0,0 +1,27 @@
+// # Code Challenges (Set 1)
+
+// 1. Write a function named fizzBuzz that takes in two(2) parameters which
+// are expected to be strings. The function should return the string Fizz if
+// the combined length of the parameters is divisible by 3, the string Buzz
+// if it is divisible by 5, and the string FizzBuzz if it is divisible by both 5 and
+// 3.
+
+function fizzBuzz(str1, str2) {
+  let str = str1.concat(str2);
+
+  if (str.length % 3 == 0 || str.length % 5 == 0) {
+    return 'FizzBuzz';
+  } else if (str.length % 5 == 0) {
+    {
+      return 'Buzz';
+    }
+  } else if (str.length % 3 == 0) {
+    return 'Fizz';
+  }
+}
+console.log(fizzBuzz('Hellooo', 'Worldone'));
+
+// 2. Write a JavaScript program that prompts a user to enter their year of
+// birth and in turn prints a string in the console stating whether the user
+// is a minor, a youth, or an elder. Anyone below 18 years is a minor,
+// anyone between 18 and 36 years is a youth and the rest are elders.
